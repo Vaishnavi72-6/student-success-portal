9bbbf3fc5795bf6b282229166534b51183e547fc
P Vaishnavi <vaishnavi.p0602@gmail.com>
2025-08-14 12:01:04 +0530

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 5000;
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`🚀 Ready on http://localhost:${port}`);
+  });
+});
